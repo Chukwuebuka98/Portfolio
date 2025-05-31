c5f6b98f74de2e568935175d2285469be33d3aa3
Chukwuebuka98 <emmanuelzico17@gmail.com>
2025-05-31 16:26:39 +0100

    resume download functionality added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -45,11 +45,12 @@ const icons = [
     target: '_blank',
   },
   {
-    href: '#blog',
+    href: '/resume.pdf',
     icon: DocsIcon,
     label: 'Resume',
     hoverColor: 'hover:text-[#2b2b2b] duration-300',
     target: '_self',
+    download: true,
   },
 ];
 
@@ -62,24 +63,27 @@ export default function Navbar() {
         transition={{ type: 'spring', stiffness: 300, damping: 20 }}
         className="fixed top-8 md:top-12 left-1/2 -translate-x-1/2 z-50 w-full max-w-[350px] md:max-w-[400px] bg-white/2 border border-white/10 hover:bg-black/50 rounded-2xl px-3 py-2.5 md:px-4 md:py-3 shadow-lg flex justify-between items-center backdrop-blur hover:duration-400 "
       >
-        {icons.map(({ href, icon: Icon, label, hoverColor, target }) => (
-          <Tooltip key={href}>
-            <TooltipTrigger asChild>
-              <Link
-                href={href}
-                aria-label={label}
-                target={target}
-                rel="noopener noreferrer"
-                className={`transition-transform hover:scale-115 hover:bg-white hover:rounded-md hover:duration-300 duration-300 p-2 ${hoverColor}`}
-              >
-                <Icon className={`w-5 h-5 md:w-6 md:h-6 `} />
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="bottom" sideOffset={6} className="text-xs">
-              {label}
-            </TooltipContent>
-          </Tooltip>
-        ))}
+        {icons.map(
+          ({ href, icon: Icon, label, hoverColor, target, download }) => (
+            <Tooltip key={href}>
+              <TooltipTrigger asChild>
+                <Link
+                  href={href}
+                  aria-label={label}
+                  target={target}
+                  rel={target === '_blank' ? 'noopener noreferrer' : undefined}
+                  download={download || undefined}
+                  className={`transition-transform hover:scale-115 hover:bg-white hover:rounded-md hover:duration-300 duration-300 p-2 ${hoverColor}`}
+                >
+                  <Icon className={`w-5 h-5 md:w-6 md:h-6 `} />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="bottom" sideOffset={6} className="text-xs">
+                {label}
+              </TooltipContent>
+            </Tooltip>
+          )
+        )}
       </motion.nav>
     </TooltipProvider>
   );
